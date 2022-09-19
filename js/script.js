77221ffe593bc64a4dbd354ed501fff2a42a4e26
Aryan salge <aryansalge02@gmail.com>
2022-09-19 23:06:34 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("p1").innerHTML = "Hello Javascript";
+document.getElementById('p1').style.color='red';
+document.getElementById('intro').style.fontSize='35px';
+document.body.style.backgroundColor  = "lightyellow";
+
+
